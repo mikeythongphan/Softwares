1c0280f68f9117a38ab05e3469d9d64ddecf979f
mikeythongphan <thong_pdt@hotmail.com>
2022-05-24 21:06:20 +0700

    Update 14-sponsor-relatives.js

--- 14-sponsor-relatives.js
+++ 14-sponsor-relatives.js
@@ -10,10 +10,51 @@ export const SponsorRelatives = (props) => {
 
   return (
     <>
-      <h5><InputLabel style={{ backgroundColor: 'black', color: 'white' }}>1. Name</InputLabel></h5>
-      <h1><InputLabel style={{ color: 'DarkGray' }}>Sponsor's name and gender</InputLabel></h1>
-      <h1><InputLabel style={{ backgroundColor: 'DarkSeaGreen', color: 'Black' }}>The sponsor is the U.S. citizen.</InputLabel></h1>
+      <h5><InputLabel style={{ backgroundColor: 'black', color: 'white' }}>17. Relatives</InputLabel></h5>
 
+      <FormControl>
+        <FormLabel id="">Do you, the Alien, have a Child, Brother or Sister living in the United States?</FormLabel>
+	<FormLabel id="">Do not include step children.</FormLabel>
+        <RadioGroup
+          row
+          name=""
+        // onChange={handleInputChange}
+        >
+          <FormControlLabel value="" control={<Radio />} label="No" />
+          <FormControlLabel value="" control={<Radio />} label="Yes" />
+        </RadioGroup>
+      </FormControl>
+
+      <h1><InputLabel style={{ color: 'DarkGray' }}>Relative 1</InputLabel></h1>
+
+      <TextField variant="outlined"
+        name=""
+        label="Relative's First and Middle Names"
+      //    value={values.applicantAlienNumber}
+      //    onChange={handleInputChange}
+      />
+
+      <TextField variant="outlined"
+        name=""
+        label="Relative's Last Name (family name)"
+      //    value={values.applicantAlienNumber}
+      //    onChange={handleInputChange}
+      />
+
+			<FormControl>
+				<FormLabel id="">If there is a suffix after the name such as Jr. or III, put that here after the last name.</FormLabel>
+<FormLabel id="">Alien's relationship to this person.</FormLabel>
+				<Select>
+					<MenuItem>Vietnam</MenuItem>
+				</Select>
+			</FormControl>
+
+			<FormControl>
+				<FormLabel id="">Relative's Status</FormLabel>
+				<Select>
+					<MenuItem>Vietnam</MenuItem>
+				</Select>
+			</FormControl>
     </>
   )
 }
