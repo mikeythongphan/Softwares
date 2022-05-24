12b261988e08b1785113c101dbaa96bd317a5424
mikeythongphan <thong_pdt@hotmail.com>
2022-05-24 21:03:32 +0700

    Update 09-sponsor-relationship.js

--- 09-sponsor-relationship.js
+++ 09-sponsor-relationship.js
@@ -10,10 +10,43 @@ export const SponsorRelationship = (props) => {
 
   return (
     <>
-      <h5><InputLabel style={{ backgroundColor: 'black', color: 'white' }}>1. Name</InputLabel></h5>
-      <h1><InputLabel style={{ color: 'DarkGray' }}>Sponsor's name and gender</InputLabel></h1>
-      <h1><InputLabel style={{ backgroundColor: 'DarkSeaGreen', color: 'Black' }}>The sponsor is the U.S. citizen.</InputLabel></h1>
+      <h5><InputLabel style={{ backgroundColor: 'black', color: 'white' }}>9. Relationship</InputLabel></h5>
+      <h1><InputLabel style={{ color: 'DarkGray' }}>Relationship</InputLabel></h1>
 
+      <TextField variant="outlined"
+        name=""
+        label="In what city did you meet the first time? Enter City Only!"
+      //    value={values.applicantGivenName}
+      //    onChange={handleInputChange}
+      />
+
+      <FormControl>
+        <FormLabel id="">It is your responsibility to get your petition to the USCIS within two years
+of the last time the two of you have physically met.</FormLabel>
+<FormLabel id="">Have you physically seen your fiancé in person within two years of filing this petition, 
+and have you allowed adequate time to get all your documents to the USCIS 
+before that two year period expires?</FormLabel>
+        <RadioGroup
+          row
+          name=""
+        // onChange={handleInputChange}
+        >
+          <FormControlLabel value="" control={<Radio />} label="No" />
+          <FormControlLabel value="" control={<Radio />} label="Yes" />
+        </RadioGroup>
+      </FormControl>
+
+      <FormControl>
+        <FormLabel id="">Were you introduced to your partner by an international marriage broker (IMB)?</FormLabel>
+        <RadioGroup
+          row
+          name=""
+        // onChange={handleInputChange}
+        >
+          <FormControlLabel value="" control={<Radio />} label="No" />
+          <FormControlLabel value="" control={<Radio />} label="Yes" />
+        </RadioGroup>
+      </FormControl>
     </>
   )
 }
