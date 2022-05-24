49640a35b9e159a600f3e91b30aeadb5b3339b71
mikeythongphan <thong_pdt@hotmail.com>
2022-05-24 21:04:01 +0700

    Update 10-sponsor-employment.js

--- 10-sponsor-employment.js
+++ 10-sponsor-employment.js
@@ -10,10 +10,104 @@ export const SponsorEmployment = (props) => {
 
   return (
     <>
-      <h5><InputLabel style={{ backgroundColor: 'black', color: 'white' }}>1. Name</InputLabel></h5>
-      <h1><InputLabel style={{ color: 'DarkGray' }}>Sponsor's name and gender</InputLabel></h1>
-      <h1><InputLabel style={{ backgroundColor: 'DarkSeaGreen', color: 'Black' }}>The sponsor is the U.S. citizen.</InputLabel></h1>
+      <h5><InputLabel style={{ backgroundColor: 'black', color: 'white' }}>10. Employment</InputLabel></h5>
+      <h1><InputLabel style={{ color: 'DarkGray' }}>Your Employment for the Past Five Years (U.S. Citizen Sponsor)</InputLabel></h1>
+      
+<InputLabel>Enter "Unemployed" or "Retired" if appropriate. More space will be provided as needed to go back 5 years.</InputLabel>
+<InputLabel>You will need a source of income or adequate assets to be approved.</InputLabel>
 
+      <TextField variant="outlined"
+        name=""
+        label="Name of Employer/Company"
+      //    value={values.applicantGivenName}
+      //    onChange={handleInputChange}
+      />
+
+      <TextField variant="outlined"
+        name=""
+        label="Occupation or Job Title"
+      //    value={values.applicantGivenName}
+      //    onChange={handleInputChange}
+      />
+
+      <TextField variant="outlined"
+        name=""
+        label="Number and street. Example: 123 Main Street"
+      //    value={values.applicantGivenName}
+      //    onChange={handleInputChange}
+      />
+
+			<FormControl>
+				<FormLabel id="">Apartment, Suite or Floor?</FormLabel>
+				<Select>
+					<MenuItem>Vietnam</MenuItem>
+				</Select>
+			</FormControl>
+
+      <TextField variant="outlined"
+        name=""
+        label="Apartment, Suite or Floor Number. Example: 43 or 532-B. Do not add "Apt" or "#"."
+      //    value={values.applicantMiddleName}
+      //    onChange={handleInputChange}
+      />
+
+      <TextField variant="outlined"
+        name=""
+        label="Town or City"
+      //    value={values.applicantMiddleName}
+      //    onChange={handleInputChange}
+      />
+
+			<FormControl>
+				<FormLabel id="">Country</FormLabel>
+				<Select>
+					<MenuItem>Vietnam</MenuItem>
+				</Select>
+			</FormControl>
+
+			<FormControl>
+				<FormLabel id="">U.S. State (Select Does Not Apply if not USA)</FormLabel>
+				<Select>
+					<MenuItem>Vietnam</MenuItem>
+				</Select>
+			</FormControl>
+
+      <TextField variant="outlined"
+        name=""
+        label="Province"
+      //    value={values.applicantMiddleName}
+      //    onChange={handleInputChange}
+      />
+
+      <FormControl>
+        <FormGroup>
+          <FormControlLabel value="" control={<Checkbox />} label="Does Not Apply" />
+        </FormGroup>
+      </FormControl>
+
+      <TextField variant="outlined"
+        name=""
+        label="Postal Code"
+      //    value={values.applicantMiddleName}
+      //    onChange={handleInputChange}
+      />
+
+			<FormControl>
+				<FormLabel id="">Pick the category that best describes your job.</FormLabel>
+				<Select>
+					<MenuItem>Vietnam</MenuItem>
+				</Select>
+			</FormControl>
+
+
+			<LocalizationProvider dateAdapter={DateAdapter}>
+				<MobileDatePicker
+					label={"I began this job on (mm/dd/yyyy)"}
+				// value={values.applicantDateOfBirth}
+				// onChange={date => handleInputChange(convertToEventParam("", date))}
+				renderInput={(params) => <TextField {...params} />}
+				/>
+			</LocalizationProvider>
     </>
   )
 }
