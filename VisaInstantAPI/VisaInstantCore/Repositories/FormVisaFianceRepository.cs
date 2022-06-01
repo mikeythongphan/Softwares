1f8ac07d39a8426a62abcab26c237ee11e7e1258
mikeythongphan <thong_pdt@hotmail.com>
2022-06-01 15:02:05 +0700

    Update FormVisaFianceRepository.cs

--- VisaInstantAPI/VisaInstantCore/Repositories/FormVisaFianceRepository.cs
+++ VisaInstantAPI/VisaInstantCore/Repositories/FormVisaFianceRepository.cs
@@ -1,1 +1,17 @@
+using OneWitsCore.UnitOfWork;
+using VisaInstantCore.Abstract.Repositories;
+using VisaInstantCore.DataObjects;
 
+namespace VisaInstantCore.Repositories
+{
+    public class FormVisaFianceRepository : RepositoryBase<FormVisaFiance, long>, IFormVisaFianceRepository
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        public FormVisaFianceRepository(IUnitOfWork context) : base(context)
+        {
+        }
+    }
+}
