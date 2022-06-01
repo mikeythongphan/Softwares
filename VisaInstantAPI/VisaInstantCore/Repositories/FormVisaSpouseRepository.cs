4d095637d8e8bb968fd49e51368a8c031f1f899a
mikeythongphan <thong_pdt@hotmail.com>
2022-06-01 15:03:24 +0700

    Update FormVisaSpouseRepository.cs

--- VisaInstantAPI/VisaInstantCore/Repositories/FormVisaSpouseRepository.cs
+++ VisaInstantAPI/VisaInstantCore/Repositories/FormVisaSpouseRepository.cs
@@ -1,1 +1,17 @@
+using OneWitsCore.UnitOfWork;
+using VisaInstantCore.Abstract.Repositories;
+using VisaInstantCore.DataObjects;
 
+namespace VisaInstantCore.Repositories
+{
+    public class FormVisaSpouseRepository : RepositoryBase<FormVisaSpouse, long>, IFormVisaSpouseRepository
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        public FormVisaSpouseRepository(IUnitOfWork context) : base(context)
+        {
+        }
+    }
+}
