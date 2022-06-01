651d55ec043eea532ee0cd53cde020b738999554
mikeythongphan <thong_pdt@hotmail.com>
2022-06-01 15:01:24 +0700

    Update FormEmploymentRepository.cs

--- VisaInstantAPI/VisaInstantCore/Repositories/FormEmploymentRepository.cs
+++ VisaInstantAPI/VisaInstantCore/Repositories/FormEmploymentRepository.cs
@@ -1,1 +1,17 @@
+using OneWitsCore.UnitOfWork;
+using VisaInstantCore.Abstract.Repositories;
+using VisaInstantCore.DataObjects;
 
+namespace VisaInstantCore.Repositories
+{
+    public class FormEmploymentRepository : RepositoryBase<FormEmployment, long>, IFormEmploymentRepository
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        public FormEmploymentRepository(IUnitOfWork context) : base(context)
+        {
+        }
+    }
+}
