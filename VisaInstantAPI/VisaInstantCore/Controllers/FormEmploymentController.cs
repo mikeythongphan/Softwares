08c32ce27e06534fc0c58af07607cb21ab0f3a37
mikeythongphan <thong_pdt@hotmail.com>
2022-06-01 15:04:44 +0700

    Update FormEmploymentController.cs

--- VisaInstantAPI/VisaInstantCore/Controllers/FormEmploymentController.cs
+++ VisaInstantAPI/VisaInstantCore/Controllers/FormEmploymentController.cs
@@ -1,1 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using OneWitsCore.Auth;
+using OneWitsCore.Controllers;
+using OneWitsCore.DTOs;
+using VisaInstantCore.Abstract.Repositories;
+using VisaInstantCore.DataObjects;
 
+namespace VisaInstantCore.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class FormEmploymentController : ControllerTableBase<FormEmployment, long>
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public FormEmploymentController(IFormEmploymentRepository repos)
+            : base(repos)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("Insert")]
+        public override IActionResult Insert([FromBody] FormEmployment obj)
+        {
+            obj.AccountId = AccountId;
+            Repos.Insert(obj);
+            Repos.UOW.Commit();
+            return Ok(ResponseDto.Succeed(obj));
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetByAccount")]
+        public IActionResult GetByAccount()
+        {
+            var result = Repos.GetFiltered(x => x.AccountId == AccountId && !x.Deleted).ToList();
+            return Ok(ResponseDto.Succeed(result));
+        }
+    }
+}
