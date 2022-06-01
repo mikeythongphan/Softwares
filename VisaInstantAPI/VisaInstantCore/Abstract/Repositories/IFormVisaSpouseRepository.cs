685c3bcf3996f685e19ed5e069a6d736bcd5eca2
mikeythongphan <thong_pdt@hotmail.com>
2022-06-01 15:11:51 +0700

    Update IFormVisaSpouseRepository.cs

--- VisaInstantAPI/VisaInstantCore/Abstract/Repositories/IFormVisaSpouseRepository.cs
+++ VisaInstantAPI/VisaInstantCore/Abstract/Repositories/IFormVisaSpouseRepository.cs
@@ -1,1 +1,9 @@
+using OneWitsCore.UnitOfWork;
+using VisaInstantCore.DataObjects;
 
+namespace VisaInstantCore.Abstract.Repositories
+{
+    public interface IFormVisaSpouseRepository : IRepositoryBase<FormVisaSpouse, long>
+    {
+    }
+}
