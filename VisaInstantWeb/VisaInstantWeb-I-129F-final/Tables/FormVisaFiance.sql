7212d20c29f730eedb2b6165f2c714bb4b35e5ba
mikeythongphan <thong_pdt@hotmail.com>
2022-06-27 21:09:37 +0700

    Update FormVisaFiance.sql

--- VisaInstantWeb/VisaInstantWeb-I-129F-final/Tables/FormVisaFiance.sql
+++ VisaInstantWeb/VisaInstantWeb-I-129F-final/Tables/FormVisaFiance.sql
@@ -141,7 +141,116 @@ CREATE TABLE [dbo].[FormVisaFiance] (
         label="Thành phố và quốc gia cư trú"
         label="Tên mẹ (tên trước hôn nhân)"
         label={"Ngày sinh của mẹ"}
+        label="Nơi sinh của mẹ"
+        label="Thành phố và quốc gia cư trú"
+        label="1. Tên của vợ/chồng cũ"
+        label={"Ngày sinh của vợ/chồng cũ"}
+        label="Nơi sinh của vợ/chồng cũ"
+        label={"Ngày kết hôn"}
+        label="Nơi kết hôn"
+        label="2. Tên của vợ/chồng cũ"
+        label={"Ngày sinh của vợ/chồng cũ"}
+        label="Nơi sinh của vợ/chồng cũ"
+        label={"Ngày kết hôn"}
+        label="Nơi kết hôn"
+	label="3. Tên của vợ/chồng cũ"
+        label={"Ngày sinh của vợ/chồng cũ"}
+        label="Nơi sinh của vợ/chồng cũ"
+        label={"Ngày kết hôn"}
+        label="Nơi kết hôn"
+	
+      <InputLabel> Địa chỉ của đương đơn trong 5 năm qua. Liệt kê địa chỉ hiện tại trước</InputLabel>
 
+        label="1. Số nhà và tên đường hiện tại"
+        label="Thành phố"
+        label="Tiểu bang/quận"
+        label="Zip Code"
+        label="Từ Tháng/năm"
+        label="Đến Tháng/năm"
+        label="2. Số nhà và tên đường hiện tại"
+        label="Thành phố"
+        label="Tiểu bang/quận"
+        label="Zip Code"
+        label="Từ Tháng/năm"
+        label="Đến Tháng/năm"
+	label="3. Số nhà và tên đường hiện tại"
+        label="Thành phố"
+        label="Tiểu bang/quận"
+        label="Zip Code"
+        label="Từ Tháng/năm"
+        label="Đến Tháng/năm"
+	label="4. Số nhà và tên đường hiện tại"
+        label="Thành phố"
+        label="Tiểu bang/quận"
+        label="Zip Code"
+        label="Từ Tháng/năm"
+        label="Đến Tháng/năm"
+	label="5. Số nhà và tên đường hiện tại"
+        label="Thành phố"
+        label="Tiểu bang/quận"
+        label="Zip Code"
+        label="Từ Tháng/năm"
+        label="Đến Tháng/năm"
+	
+      <InputLabel>Địa chỉ của đương đơn bên ngoài Hoa Kỳ trên một năm</InputLabel>
+
+        label="Số nhà và tên đường"
+        label="Thành phố"
+        label="Tiểu bang/quận"
+        label="Zip Code"
+        label="Từ Tháng/năm"
+        label="Đến Tháng/năm"
+
+      <InputLabel>Nơi làm việc của đương đơn trong vòng 5 năm qua. Liệt kê nơi làm việc hiện tại trước</InputLabel>
+
+        label="1. Tên và địa chỉ nơi làm việc hện tại"
+        label="Chức vụ"
+        label="Từ Tháng/năm"
+        label="Đến Tháng/năm"
+        label="2. Tên và địa chỉ nơi làm việc hện tại"
+        label="Chức vụ"
+        label="Từ Tháng/năm"
+        label="Đến Tháng/năm"
+	label="3. Tên và địa chỉ nơi làm việc hện tại"
+        label="Chức vụ"
+        label="Từ Tháng/năm"
+        label="Đến Tháng/năm"
+	label="4. Tên và địa chỉ nơi làm việc hện tại"
+        label="Chức vụ"
+        label="Từ Tháng/năm"
+        label="Đến Tháng/năm"
+	label="5. Tên và địa chỉ nơi làm việc hện tại"
+        label="Chức vụ"
+        label="Từ Tháng/năm"
+        label="Đến Tháng/năm"
+	
+      <InputLabel>Chức vụ công việc ở nước ngoài nếu không có ở trên</InputLabel>
+
+        label="Tên và địa chỉ nơi làm việc"
+        label="Chức vụ"
+        label="Từ Tháng/năm"
+        label="Đến Tháng/năm"
+
+      <h5><InputLabel style={{ textDecorationLine: 'underline', fontWeight: 'bold', color: 'blue' }}>Part C - Hồ sơ cần thiết:</InputLabel></h5>
+
+        <FormLabel id="">Người thụ hưởng (và con cái):</FormLabel>
+          <FormControlLabel value="" checked={true} control={<Checkbox />} label="1. Form I-94 và bản sao hộ chiếu của từng người" />
+          <FormControlLabel value="" checked={true} control={<Checkbox />} label="2. Giấy khai sinh (hoặc bất kỳ giấy tờ nào thay thế) của từng người" />
+          <FormControlLabel value="" checked={true} control={<Checkbox />} label="3. Tất cả những biên nhận, quyết định từ chối hồ sơ, giấy tờ i-20… trước đây của từng người" />
+          <FormControlLabel value="" checked={true} control={<Checkbox />} label="4. Hồ sơ phạm tội (nếu có) của từng người" />
+          <FormControlLabel value="" checked={true} control={<Checkbox />} label="5. 02 tấm hình passport của từng người" />
+          <FormControlLabel value="" checked={true} control={<Checkbox />} label="6. Giấy tờ ly hôn trước đây của người thụ hưởng" />
+          <FormControlLabel value="" checked={true} control={<Checkbox />} label="7. Giấy tờ liên quan đến việc đổi tên (nếu có)" />
+	  
+	          <FormLabel id="">Đương đơn:</FormLabel>
+          <FormControlLabel value="" checked={true} control={<Checkbox />} label="8. Thư xác nhận của nơi làm việc về việc đương đơn đang làm việc" />
+          <FormControlLabel value="" checked={true} control={<Checkbox />} label="9. Sao kê tài khoản ngân hàng trong 3 năm gần nhất" />
+          <FormControlLabel value="" checked={true} control={<Checkbox />} label="10. Giấy tờ khai thuế trong 3 năm gần nhất" />
+          <FormControlLabel value="" checked={true} control={<Checkbox />} label="11. W-2, Paystubs, và Checks trong vòng 3 tháng" />
+          <FormControlLabel value="" checked={true} control={<Checkbox />} label="12. Giấy tờ thể hiện là công dân Mỹ (hộ chiếu Mỹ, giấy chứng nhận quốc tịch)" />
+          <FormControlLabel value="" checked={true} control={<Checkbox />} label="13. 02 tấm hình hộ chiếu" />
+          <FormControlLabel value="" checked={true} control={<Checkbox />} label="14. Giấy tờ ly hôn trước đây" />
+	  
     	[Deleted] BIT CONSTRAINT [DF_FormVisaFiance_Deleted] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_FormVisaFiance] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
