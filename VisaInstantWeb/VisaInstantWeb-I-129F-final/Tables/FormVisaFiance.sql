ff59d4009afd042daf4042127ec0dcb48315d0e6
mikeythongphan <thong_pdt@hotmail.com>
2022-06-28 09:53:02 +0700

    Update FormVisaFiance.sql

--- VisaInstantWeb/VisaInstantWeb-I-129F-final/Tables/FormVisaFiance.sql
+++ VisaInstantWeb/VisaInstantWeb-I-129F-final/Tables/FormVisaFiance.sql
@@ -220,26 +220,20 @@ Sponsor5NameAddressWork
 Sponsor5Position
 Sponsor5FromMonthYear
 Sponsor5ToMonthYear
-
-      <h5><InputLabel style={{ textDecorationLine: 'underline', fontWeight: 'bold', color: 'blue' }}>Part C - Hồ sơ cần thiết:</InputLabel></h5>
-
-        <FormLabel id="">Người thụ hưởng (và con cái):</FormLabel>
-          <FormControlLabel value="" checked={true} control={<Checkbox />} label="1. Form I-94 và bản sao hộ chiếu của từng người" />
-          <FormControlLabel value="" checked={true} control={<Checkbox />} label="2. Giấy khai sinh (hoặc bất kỳ giấy tờ nào thay thế) của từng người" />
-          <FormControlLabel value="" checked={true} control={<Checkbox />} label="3. Tất cả những biên nhận, quyết định từ chối hồ sơ, giấy tờ i-20… trước đây của từng người" />
-          <FormControlLabel value="" checked={true} control={<Checkbox />} label="4. Hồ sơ phạm tội (nếu có) của từng người" />
-          <FormControlLabel value="" checked={true} control={<Checkbox />} label="5. 02 tấm hình passport của từng người" />
-          <FormControlLabel value="" checked={true} control={<Checkbox />} label="6. Giấy tờ ly hôn trước đây của người thụ hưởng" />
-          <FormControlLabel value="" checked={true} control={<Checkbox />} label="7. Giấy tờ liên quan đến việc đổi tên (nếu có)" />
-	  
-	          <FormLabel id="">Đương đơn:</FormLabel>
-          <FormControlLabel value="" checked={true} control={<Checkbox />} label="8. Thư xác nhận của nơi làm việc về việc đương đơn đang làm việc" />
-          <FormControlLabel value="" checked={true} control={<Checkbox />} label="9. Sao kê tài khoản ngân hàng trong 3 năm gần nhất" />
-          <FormControlLabel value="" checked={true} control={<Checkbox />} label="10. Giấy tờ khai thuế trong 3 năm gần nhất" />
-          <FormControlLabel value="" checked={true} control={<Checkbox />} label="11. W-2, Paystubs, và Checks trong vòng 3 tháng" />
-          <FormControlLabel value="" checked={true} control={<Checkbox />} label="12. Giấy tờ thể hiện là công dân Mỹ (hộ chiếu Mỹ, giấy chứng nhận quốc tịch)" />
-          <FormControlLabel value="" checked={true} control={<Checkbox />} label="13. 02 tấm hình hộ chiếu" />
-          <FormControlLabel value="" checked={true} control={<Checkbox />} label="14. Giấy tờ ly hôn trước đây" />
+BeneficiaryProfileFormI94
+BeneficiaryProfileBirthCertificate
+BeneficiaryProfileI20Documents
+BeneficiaryProfileCriminalRecord
+BeneficiaryProfilePassportPhotos
+BeneficiaryProfilePreviousDivorce
+BeneficiaryProfilePapersRelated
+SponsorProfileLetterConfirmation
+SponsorProfileBankStatement
+SponsorProfileTaxReturns
+SponsorProfileW2PaystubsChecks
+SponsorProfileDocumentsShowing
+SponsorProfilePassportPhotos
+SponsorProfilePreviousDivorce
 	  
     	[Deleted] BIT CONSTRAINT [DF_FormVisaFiance_Deleted] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_FormVisaFiance] PRIMARY KEY CLUSTERED ([Id] ASC)
